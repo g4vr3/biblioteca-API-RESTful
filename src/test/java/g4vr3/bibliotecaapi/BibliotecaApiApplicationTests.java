14446c6bc700aabb0e9ce637b43cb5d5067344d1
g4vr3 <gdfizan@gmail.com>
2025-01-21 17:08:13 +0100

    Initial Commit

---
+++ src/test/java/g4vr3/bibliotecaapi/BibliotecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package g4vr3.bibliotecaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
